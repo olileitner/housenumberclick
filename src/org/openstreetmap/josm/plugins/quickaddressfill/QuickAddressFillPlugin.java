3faf4df36379a4b2bce83f458353cef326b3772b
Oliver Leitner <oliver@familie-leitner.de>
2026-04-03 08:55:31 +0200

    Auto-add QuickAddressFill toolbar button on first run

--- src/org/openstreetmap/josm/plugins/quickaddressfill/QuickAddressFillPlugin.java
+++ src/org/openstreetmap/josm/plugins/quickaddressfill/QuickAddressFillPlugin.java
@@ -1,14 +1,34 @@
 package org.openstreetmap.josm.plugins.quickaddressfill;
 
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.preferences.ToolbarPreferences;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.spi.preferences.Config;
 
 public class QuickAddressFillPlugin extends Plugin {
 
+    private static final String TOOLBAR_ID = "quickaddressfill";
+    private static final String TOOLBAR_MIGRATION_PREF = "quickaddressfill.toolbar.button.added.v1";
+
     public QuickAddressFillPlugin(PluginInformation info) {
         super(info);
-        MainApplication.getMenu().dataMenu.add(new QuickAddressFillAction());
+        QuickAddressFillAction action = new QuickAddressFillAction();
+        MainApplication.getMenu().dataMenu.add(action);
+        ensureToolbarButtonAddedOnce();
+    }
+
+    private void ensureToolbarButtonAddedOnce() {
+        if (Config.getPref().getBoolean(TOOLBAR_MIGRATION_PREF, false)) {
+            return;
+        }
+
+        ToolbarPreferences toolbar = MainApplication.getToolbar();
+        if (toolbar == null) {
+            return;
+        }
+
+        toolbar.addCustomButton(TOOLBAR_ID, -1, false);
+        Config.getPref().putBoolean(TOOLBAR_MIGRATION_PREF, true);
     }
 }
-
